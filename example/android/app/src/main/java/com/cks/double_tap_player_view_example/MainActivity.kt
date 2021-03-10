2f1f3945fcb5e24b666069473d1751c627543c12
hiroyukitamura <hiroyuktamura@gmail.com>
2021-03-10 22:51:23 +0900

    remove unused native source

--- example/android/app/src/main/java/com/cks/double_tap_player_view_example/MainActivity.kt
+++ example/android/app/src/main/java/com/cks/double_tap_player_view_example/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.cks.double_tap_player_view_example;
+package com.cks.double_tap_player_view_example
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
-}
+class MainActivity : FlutterActivity()
